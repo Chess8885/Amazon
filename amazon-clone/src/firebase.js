b9309ee6821d71355a9caf10a953c1a0b4fb1936
toto42 <alexandre.ohayon@epitech.eu>
2021-01-04 09:42:04 +0100

    [ADD] firebase connect

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5IE2Z_OTC7bwr0zhQ03YaTWtp9TkS7P0",
+    authDomain: "clone-77e31.firebaseapp.com",
+    projectId: "clone-77e31",
+    storageBucket: "clone-77e31.appspot.com",
+    messagingSenderId: "425357464671",
+    appId: "1:425357464671:web:cb105736efad5d40e4b7ea"
+  };
