00a7fb29c7bf0aced16392b13ca00989bfafe92f
toto42 <alexandre.ohayon@epitech.eu>
2021-01-04 11:07:52 +0100

    [ADD] basket icon

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
     return (
@@ -43,6 +44,11 @@ function Header() {
             Prime
             </span>
         </div>
+
+<div className="header_optionBasket">
+    <ShoppingBasketIcon />
+    <span className="header_optionLineTwo header_basketCount">0</span>
+</div>
 
         </div>
     );
