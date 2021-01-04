339d6fed97a750c983dce3db7fa4ed3632992304
toto42 <alexandre.ohayon@epitech.eu>
2021-01-04 10:08:51 +0100

    [ADD] header basic

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello consumers, lets build amazon store !</h1>
+      <Header />
+      {}
+      {}
     </div>
   );
 }
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,12 +1,56 @@
 import React from 'react'
-import './Header.css'
 
 function Header() {
     return (
         <div className='header'>
-            
+            <img src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+        <div className="header_search">
+
+            <input className="header_searchInput" type="text" />
+
         </div>
-    )
+
+        <div className="header_nav">
+        <div className="header_option">
+            <span className="header_optionLineOne">
+            Hello, guest
+            </span>
+        </div>
+
+        <div className="header_option">
+            <span className="header_optionLineTwo">
+            Sign In
+            </span>
+        </div>
+
+        </div>
+
+        <div className="header_option">
+            <span className="header_optionLineOne">
+           Returns
+            </span>
+        </div>
+
+        <div className="header_option">
+            <span className="header_optionLineTwo">
+            Orders
+            </span>
+        </div>
+
+        <div className="header_option">
+            <span className="header_optionLineOne">
+            Your
+            </span>
+        </div>
+
+        <div className="header_option">
+            <span className="header_optionLineTwo">
+            Prime
+            </span>
+        </div>
+
+        </div>
+    );
 }
 
 export default Header
