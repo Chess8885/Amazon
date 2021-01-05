f56ecd0cb295535f686a189a1b8fa8846981e9c5
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2021-01-05 15:39:16 +0100

    [ADD] img of first product

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -7,7 +7,13 @@ function Product() {
             <div className="product_info">
                 <p>The lean startup</p>
                 <p className="product_price"><small>$</small><strong>11.99</strong></p>
+                <div className="product_rating">
+                    <p></p>
+
+                </div>
             </div>
+
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL.AC_SY400_.jpg" alt=""/>
         </div>
     )
 }
