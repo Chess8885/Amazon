36cc27e595df11deb995099256a3ac65f05fc5d3
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2021-01-05 15:33:20 +0100

    [ADD] first product

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -4,7 +4,10 @@ import './Product.css'
 function Product() {
     return (
         <div className="product">
-            
+            <div className="product_info">
+                <p>The lean startup</p>
+                <p className="product_price"><small>$</small><strong>11.99</strong></p>
+            </div>
         </div>
     )
 }
