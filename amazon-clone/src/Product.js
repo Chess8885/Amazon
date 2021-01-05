83bc725891b44421680c21662578478da1902d58
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2021-01-05 16:21:35 +0100

    [ADD] remove hardcode title product

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -5,7 +5,7 @@ function Product({title, image, price, rating }) {
     return (
         <div className="product">
             <div className="product_info">
-                <p>The lean startup</p>
+                <p>{ title }</p>
                 <p className="product_price"><small>$</small><strong>11.99</strong></p>
                 <div className="product_rating">
                     <p>⭐</p>
