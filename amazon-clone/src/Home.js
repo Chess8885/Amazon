743763c61ca7b64b6f01a4baf83d1f0dc0207491
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2021-01-06 09:14:27 +0100

    [ADD] second product and id for products

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -9,10 +9,22 @@ function Home() {
                 <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="" />
             
 
-            <div className="home_row">
-                <Product title="The lean startup" price={12.99} image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" rating={5} />
-                <Product/>
-            </div>
+                <div className="home_row">
+          <Product
+            id="12321341"
+            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={11.96}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+          />
+          <Product
+            id="49538094"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+          />
+        </div>
 
             <div className="home_row">
             <Product/>
