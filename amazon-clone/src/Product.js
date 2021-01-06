07b0e75c59d69c981317584acbf1172fc8d84683
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2021-01-06 09:41:09 +0100

    [ADD] use state basket

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Product.css'
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
     return (
         <div className="product">
             <div className="product_info">
