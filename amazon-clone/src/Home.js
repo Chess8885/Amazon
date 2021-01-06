8e82fc08246da24f83d8f642f0a84c5158e8b56f
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2021-01-06 09:48:39 +0100

    [ADD] classname __

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -5,11 +5,11 @@ import Product from './Product';
 function Home() {
     return (
         <div className="home">
-            <div className="home_container">
+            <div className="home__container">
                 <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="" />
             
 
-                <div className="home_row">
+                <div className="home__row">
           <Product
             id="12321341"
             title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
@@ -26,7 +26,7 @@ function Home() {
           />
         </div>
 
-            <div className="home_row">
+            <div className="home__row">
             <Product
             id="4903850"
             title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
@@ -51,7 +51,7 @@ function Home() {
             </div>
 
 
-            <div className="home_row">
+            <div className="home__row">
             <Product
             id="90829332"
             title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
